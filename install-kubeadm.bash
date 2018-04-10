d6c7ae32e64ced408889de92b1d696e7c1e1575c
Tiffany Timbers <tiffany.timbers@gmail.com>
2018-04-10 15:20:15 -0700

    identifying where we need to switch from ubuntu to CentOS commands

--- install-kubeadm.bash
+++ install-kubeadm.bash
@@ -1,16 +1,20 @@
 #!/bin/bash
-apt-get update
-apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+# sudo yum update - not using because unlike apt-get, yum automatically refreshes the list of packages (right?)
+# sudo yum install -y apt-transport-https won't work because doesn't exist for CentOS - try varnish?
+
+## edit apt-key on lines below to rpm something?
+#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable
+
+# This won't work because line 3 isn't working...
+# deb http://apt.kubernetes.io/ kubernetes-xenial main
+# deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable
 EOF
 
-apt-get update
+# sudo yum update - not using because unlike apt-get, yum automatically refreshes the list of packages (right?)
 
-apt-get install -y docker-ce=17.03.2~ce-0~ubuntu-xenial
+yum install -y docker-ce=17.03.2~ce-0~ubuntu-xenial # is it OK to use ubuntu in our Docker container? Probably...
 
 systemctl stop docker
 modprobe overlay
@@ -19,7 +23,7 @@ rm -rf /var/lib/docker/*
 systemctl start docker
 
 # Install kubernetes components!
-apt-get install -y \
+yum install -y \
         kubelet=1.9.2-00 \
         kubeadm=1.9.2-00 \
         kubernetes-cni=0.6.0-00
